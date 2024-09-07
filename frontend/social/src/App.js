4498a12899969d9db7159392314e2851a5a9bbcc
mahmoudnabil133 <hodanabil144@gmail.com>
2024-09-08 00:49:14 +0300

    created ProtectRoutes component to protect client routes

--- frontend/social/src/App.js
+++ frontend/social/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import React from 'react'
+import Login from './pages/login';
+import SignUp from './pages/signup';
+import Profile from './pages/profile';
+import ProtectRoutes from './components/protectRoutes';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className='app'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>} />
+          <Route path='/signup' element={<SignUp/>} />
+          <Route path='/profile' element={
+            <ProtectRoutes>
+              < Profile/>
+            </ProtectRoutes>
+            } />
+          <Route path='*' element={<h1>Not Found</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
