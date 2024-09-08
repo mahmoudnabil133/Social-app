a87fd0dc9b82a3d245b3496c4393b94e95a1b573
mahmoudnabil133 <hodanabil144@gmail.com>
2024-09-09 00:17:45 +0300

    Fix: issue of missing type of type attribute in likeSchema

--- backend/models/like.js
+++ backend/models/like.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 // const post = require('./post');
 const likeSchema = new mongoose.Schema({
     type:{
-        enum: ['like', 'dislike', 'love', 'haha']
+        type: String,
+        enum: {
+            values: ['Like', 'Dislike', 'Love', 'Haha']
+        },
+        default: 'Like'
     },
     post: {
         type: mongoose.Schema.Types.ObjectId,
