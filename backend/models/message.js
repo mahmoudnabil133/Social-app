e977ce000f46cddef0e78efe7b10b9481b07c2b1
mahmoudnabil133 <hodanabil144@gmail.com>
2024-09-11 12:03:36 +0300

    Add message model to save & do crud on messages

---
+++ backend/models/message.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    from:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    to:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message:{
+        type: String,
+        required: [true, 'please send a message']
+    },
+    timeStamps:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
