255ce9165270948f711aa7353c4ee60f45478567
mahmoudnabil133 <hodanabil144@gmail.com>
2024-09-13 03:31:57 +0300

    Add email worker to process emails

---
+++ backend/workers/emailWorker.js
@@ -0,0 +1,18 @@
+const Queue = require('bull');
+const userQueue = new Queue('user-queue');
+const sendEmail = require('../utils/sendEmail');
+
+const processSendingEmail = async()=>{
+    userQueue.process(async(job)=>{
+        const {email, subject, emailBodyHtml} = job.data;
+        await sendEmail(email, subject, emailBodyHtml);
+        userQueue.on('completed', (job, result)=>{
+            console.log(`job :${job.id} completed`);
+        });
+        userQueue.on('failed', (job, err)=>{
+            console.log(`job ${job.id}failed: ${err.message}`);
+        });
+    })
+};
+
+module.exports = processSendingEmail;
---
+++ backend/workers/msgWorker.js
@@ -0,0 +1,27 @@
+const Queue = require('bull');
+const {getIo} = require('../utils/io');
+const Message = require('../models/message');
+
+const processMessage = async()=>{
+    const messageQueue = new Queue('message-queue');
+    // console.log('worker is running');
+    messageQueue.process(async(job)=>{
+        const {data} = job.data;
+        const message = await Message.create({
+            from: data.from,
+            to: data.recievedId,
+            message: data.message,
+            timeStamps: data.timeStamps
+        });
+        const io = getIo();
+        io.to(data.recievedId).emit('recieve-message', data);
+    });
+    messageQueue.on('completed', (job, result)=>{
+        console.log(`job :${job.id} completed`);
+    });
+    messageQueue.on('failed', (job, err)=>{
+        console.log(`job ${job.id}failed: ${err.message}`);
+    });
+
+};
+module.exports = processMessage;
