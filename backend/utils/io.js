e0ef2e83f4e375f333b97b81a4b814987423ad60
mahmoudnabil133 <hodanabil144@gmail.com>
2024-09-13 10:57:52 +0300

    allow cors from production origin

--- backend/utils/io.js
+++ backend/utils/io.js
@@ -3,7 +3,7 @@ let io;
 const initializeSocket = (server)=>{
     io = new Server(server, {
         cors: {
-            origin: 'http://localhost:3000',
+            origin: '*',
         }
     });
     return io;
