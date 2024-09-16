0f67e99671ff869f7dfa68c9d6404b7b3c0d29fd
Ubuntu <mahmoudnabil@linux-vm01.2aawcqzsgcpudjh0yvzkoriz0b.dohx.internal.cloudapp.net>
2024-09-16 23:55:39 +0000

    upload server permenet

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,25 @@ processMessage();
 processEmail();
 
 // middle ware
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  'http://www.mahmoudnabil.tech:3000',
+	'http://localhost:3000',
+  'https://social-app-git-main-mahmoudnabil133s-projects.vercel.app'
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // Check if the incoming origin is in the allowedOrigins list
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(cookieParser());
