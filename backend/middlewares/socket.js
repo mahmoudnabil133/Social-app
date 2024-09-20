1f4a1982da779fc2b220eb314ecc3782b49f90ae
mahmoudnabil133 <hodanabil144@gmail.com>
2024-09-21 02:10:02 +0300

    use token log to check socket connection

--- backend/middlewares/socket.js
+++ backend/middlewares/socket.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const authSocket = (io)=>{
     return io.use((socket, next)=>{
         const token = socket.handshake.auth.token;
+        console.log('token', token);
         if (token){
             jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
                 if(err){
